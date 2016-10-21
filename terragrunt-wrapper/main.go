fc909289e140bad815d36d90114c6691699f1ca4
Stein Inge Morisbak <stein.inge.morisbak@BEKK.no>
2016-10-21 07:49:54 +0200

    added check for installed terraform and terragrunt

--- terragrunt-wrapper/main.go
+++ terragrunt-wrapper/main.go
@@ -2,11 +2,11 @@ package main
 
 import (
 	"fmt"
+        "strings"
 	"github.com/nsbno/cloud-tools/config"
 	"github.com/nsbno/cloud-tools/wrapper"
 	"os"
 	"os/exec"
-	"regexp"
 	"time"
 )
 
@@ -16,10 +16,17 @@ import (
 func main() {
 	config := config.ParseDefaultCloudConfig()
 
-	if !isRequiredTerraformVersion(config.TerraformVersion) {
-		fmt.Fprintf(os.Stderr, "Bad Terraform version - cloud-config.yml requires %s\n", config.TerraformVersion)
-		os.Exit(1)
+//	if !isRequiredTerraformVersion(config.TerraformVersion) {
+//		fmt.Fprintf(os.Stderr, "Bad Terraform version - cloud-config.yml requires %s\n", config.TerraformVersion)
+//		os.Exit(1)
+//	}
+
+	if !isInstalled("terraform") {
+		fmt.Fprintf(os.Stderr, "Terraform is not installed!")
 	}
+        if !isInstalled("terragrunt") {
+                fmt.Fprintf(os.Stderr, "Terraform is not installed!")
+        }
 
 	start := time.Now()
 	fmt.Println("Started terragrunt operation at:", start)
@@ -33,11 +40,20 @@ func main() {
 	fmt.Println("Total duration (seconds): ", duration)
 }
 
-func isRequiredTerraformVersion(requiredVersion string) bool {
-	cmd := exec.Command("terraform", "--version")
-	output, _ := cmd.Output()
-	text := string(output)
-	terraformVersion, _ := regexp.Compile(`Terraform v(\d{1,2}\.\d{1,2}\.\d{1,2})`)
-	result_slice := terraformVersion.FindStringSubmatch(text)
-	return result_slice[1] == requiredVersion
+func isInstalled(execName string) bool {
+      cmd := exec.Command(execName, "--version")
+      output, _ := cmd.Output()
+      text := string(output)
+      return strings.Contains(text, execName)
 }
+
+
+
+//func isRequiredTerraformVersion(requiredVersion string) bool {
+//	cmd := exec.Command("terraform", "--version")
+//	output, _ := cmd.Output()
+//	text := string(output)
+//	terraformVersion, _ := regexp.Compile(`Terraform v(\d{1,2}\.\d{1,2}\.\d{1,2})`)
+//	result_slice := terraformVersion.FindStringSubmatch(text)
+//	return result_slice[1] == requiredVersion
+//}
