bfa8a975d883ef3651a9d52fdfa764a12f7c23a6
Stein Inge Morisbak <stein.inge.morisbak@BEKK.no>
2017-03-22 16:11:21 +0100

    terraform-wrapper

---
+++ terraform-wrapper/main.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"fmt"
+        "strings"
+	"regexp"
+	"github.com/nsbno/cloud-tools/config"
+	"github.com/nsbno/cloud-tools/wrapper"
+	"os"
+	"os/exec"
+	"time"
+)
+
+// terraform-wrapper will get secrets from your pass password store,
+// setup an environment containing secrets and execute terraform,
+// passing command-line arguments to terraform as-is
+func main() {
+        if !isInstalled("terraform") {
+                fmt.Fprintf(os.Stderr, "Terraform is not installed!\n")
+                os.Exit(1)
+        }
+        if !isInstalled("pass") {
+                fmt.Fprintf(os.Stderr, "pass is not installed!\n")
+                os.Exit(1)
+        }
+
+	config := config.ParseDefaultCloudConfig()
+
+//	if !isRequiredTerraformVersion(config.TerraformVersion) {
+//		fmt.Fprintf(os.Stderr, "Bad Terraform version - cloud-config.yml requires %s\n", config.TerraformVersion)
+//		os.Exit(1)
+//	}
+
+        // Always add AWS credentials
+        var credentials []string
+        credentials = append(credentials, "AWS_ACCESS_KEY_ID="+os.Getenv("AWS_ACCESS_KEY_ID"))
+        credentials = append(credentials, "AWS_SECRET_ACCESS_KEY="+os.Getenv("AWS_SECRET_ACCESS_KEY"))
+
+	secEnv := wrapper.GetEnvironmentVariablesForSecrets(config.SecretVariables[:])
+        secEnv = append(secEnv, credentials...)
+	env := wrapper.GetEnvironmentVariablesForValues(config.Variables[:])
+        start := time.Now()
+        fmt.Println("Started terraform operation at:", start)
+        wrapper.RunCmds(config.Commands[:])
+        fullEnv := append(env, secEnv...)
+	wrapper.ExecuteCommand("terraform", os.Args[1:], fullEnv)
+	stop := time.Now()
+	fmt.Println("Started terraform operation at:", start)
+	fmt.Println("Finished terraform operation at:", stop)
+	duration := stop.Unix() - start.Unix()
+	fmt.Println("Total duration (seconds): ", duration)
+}
+
+func isInstalled(execName string) bool {
+	cmd := exec.Command(execName, "--version")
+	output, _ := cmd.Output()
+        totest :=  strings.ToLower(string(output))
+	if strings.Contains(totest, execName) {
+		return true
+	}
+	return false
+}
+
+
+
+func isRequiredTerraformVersion(requiredVersion string) bool {
+	cmd := exec.Command("terraform", "--version")
+	output, _ := cmd.Output()
+	text := string(output)
+	terraformVersion, _ := regexp.Compile(`Terraform v(\d{1,2}\.\d{1,2}\.\d{1,2})`)
+	result_slice := terraformVersion.FindStringSubmatch(text)
+	return result_slice[1] == requiredVersion
+}
