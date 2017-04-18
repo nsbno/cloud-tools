c462a8f0f51934ef7de0e2e451950d315865b10b
Kjetil Valle <kjetil.valle@gmail.com>
2017-04-18 14:15:24 +0200

    Add script for fetching AWS info (like IPs) for a given environment

---
+++ bin/describe_aws.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+the_env=$1
+
+if [[ -z $the_env ]]; then
+	echo "Usage: describe_aws ENV"
+	exit 1
+fi
+
+function print_label() {
+	echo -ne "$1:\t[fetching]\b\b\b\b\b\b\b\b\b\b"	
+}
+
+function print_value() {
+	read value
+	echo -en "          \b\b\b\b\b\b\b\b\b\b"
+	echo -e "$value" | tr -d '"'
+}
+
+echo "Information about $the_env VPC:"
+echo
+
+print_label "Bastion server, private IP"
+envchain aws aws ec2 describe-instances --filters "Name=tag-value, Values=${the_env}_bastion" \
+ | jq ".Reservations[].Instances[].NetworkInterfaces[].PrivateIpAddress" \
+ | print_value
+
+print_label "Bastion server, public IP"
+envchain aws aws ec2 describe-instances --filters "Name=tag-value, Values=${the_env}_bastion" \
+ | jq ".Reservations[].Instances[].NetworkInterfaces[].Association.PublicIp" \
+ | print_value
+
+print_label "Managment server, private IP"
+envchain aws aws ec2 describe-instances --filters "Name=tag-value, Values=${the_env}_mgmtserver" \
+ | jq ".Reservations[].Instances[].NetworkInterfaces[].PrivateIpAddress" \
+ | print_value
